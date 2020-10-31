48324c42020588794be482a33392a400ec20968b
danakristhel <danakristhel@gmail.com>
2020-10-31 15:52:45 -0300

    axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import React, {useEffect, useState} from 'react';
+
 import './App.css';
 import Tweet from './Tweet'
+import axios from 'axios'
 
 function App() {
+
+  const [data, setData] = useState([])
+
+const fetchData = async () => {
+  try {
+    const response = await axios.get('https://coursera-twitter-api.herokuapp.com/tweets')
+    setData(response.data)
+  }
+catch(error){
+  console.error(error);
+}
+}
+useEffect(() => {
+console.log("here")
+  fetchData()
+})
+
   return (
-    <Tweet text ='text passed down' author = 'Dana' date = '2020/10/31'/>
-  )
+    <div>
+      {data.map(tweet => <Tweet key={tweet.id} {...tweet} />)}
+    </div>
+    );
 }
 
 export default App;
